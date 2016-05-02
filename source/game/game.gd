e6d5a717ae844a99d665e5fc07e7d04cd3120abb
Mounir Ybanez <rinuom91@gmail.com>
2016-05-02 16:49:17 +0800

    Added setting up of pieces on the board.
    - Adjusted the position of the tilemap tha results to the right and left margin of the board.
    - Mistakenly, the board had 10 columns. The board now set to 9

--- source/game/game.gd
+++ source/game/game.gd
@@ -1,7 +1,34 @@
 
 extends Node2D
 
+onready var global = get_node("/root/global")
+
 func _ready():
-	pass
+	setup_black()
+	setup_white()
+
+func setup_pieces(type, y_sign, row_start, row_increment):
+	var pieces = global.get_player_pieces(type)
+	var row = row_start
+	var col = 0
+	var h = 64
+	var w = 72
+	var y_offset = 64 * y_sign
+	var x_offset = 36
+	for piece in pieces:
+		var x = (col * w) + x_offset
+		var y = (row * h) + y_offset
+		var pos = Vector2(x, y)
+		piece.set_pos(pos)
+		add_child(piece)
+		col += 1
+		if col >= 9:
+			row += row_increment
+			col = 0
+
+func setup_black():
+	setup_pieces("black", 1, 0, 1) 
 
 
+func setup_white():
+	setup_pieces("white", -1, 9, -1)
