f87cb7d90f5655a7c1db84a729aed2aaa847f693
Mounir Ybanez <rinuom91@gmail.com>
2016-05-05 15:57:11 +0800

    Added draggable and reveal options.
    - Added setting of cover.
    - Added option to set the piece draggable or not.
    - Added option to reveal the piece appearance.
    - Cover will be seen if appearance is hidden.

--- source/game/piece.gd
+++ source/game/piece.gd
@@ -7,6 +7,7 @@ signal piece_on_clash(piece1, piece2)
 var type
 var rank
 var special_rank_kill
+var drag_enable = true
 
 func set_type(what):
 	type = what
@@ -20,6 +21,19 @@ func set_rank(what):
 func get_rank():
 	return rank
 
+func set_cover(path):
+	set_texture(load(path))
+
+func enable_drag(enable):
+	drag_enable = enable
+
+func is_drag_enable():
+	return drag_enable
+
+func reveal(show):
+	var appearance = get_node("appearance")
+	appearance.set_hidden(!show)
+
 func set_region_position(pos):
 	var appearance = get_node("appearance")
 	var size = Vector2(72, 64)
@@ -27,12 +41,13 @@ func set_region_position(pos):
 	appearance.set_region_rect(rect)
 
 func get_drag_data(pos):
-	var icon = load("res://source/game/piece.scn").instance()
-	var appearance = get_node("appearance")
-	var region = appearance.get_region_rect()
-	icon.set_region_position(region.pos)
-	set_drag_preview(icon)
-	return self
+	if drag_enable:
+		var icon = load("res://source/game/piece.scn").instance()
+		var appearance = get_node("appearance")
+		var region = appearance.get_region_rect()
+		icon.set_region_position(region.pos)
+		set_drag_preview(icon)
+		return self
 
 func can_drop_data(pos, data):
 	return true
