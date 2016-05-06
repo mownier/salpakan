aab1e08e6e9496edf5507ad2dcf41a2c096efae9
Mounir Ybanez <rinuom91@gmail.com>
2016-05-06 16:57:21 +0800

    Added player class.

---
+++ source/common/player.gd
@@ -0,0 +1,25 @@
+
+extends Reference
+
+var connection_id
+var name
+var color
+
+func set_connection_id(id):
+	connection_id = id
+
+func get_connection_id():
+	return connection_id
+
+func set_name(player_name):
+	name = player_name
+
+func get_name():
+	return name
+
+func set_color(piece_color):
+	color = piece_color
+
+func get_color():
+	return color
+
